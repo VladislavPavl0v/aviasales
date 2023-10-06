7fa2083f9283363799dda14185c751046f95c5b4
vladidance86@gmail.com <vladidance86@gmail.com>
2023-10-06 12:33:21 +0300

    задание 3.4.15

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -1,46 +1,86 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import propTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter } from '../../store/aviasalesSlice';
 import styles from './Filter.module.scss';
 
+function FilterItem({ value, label, checked, onClick }) {
+  return (
+    <li className={styles.checkbox}>
+      <label className={styles.checkbox__label}>
+        <input
+          className={styles.checkbox__real}
+          type="checkbox"
+          checked={checked}
+          onChange={() => onClick(!checked, value)}
+        />
+        <span className={styles.checkbox__unreal} />
+        {label}
+      </label>
+    </li>
+  );
+}
+
+FilterItem.propTypes = {
+  value: propTypes.string,
+  label: propTypes.string,
+  checked: propTypes.bool,
+  onClick: propTypes.func,
+};
+
+FilterItem.defaultProps = {
+  value: undefined,
+  label: undefined,
+  checked: false,
+  onClick: () => {},
+};
+
 function Filter() {
+  const dispatch = useDispatch();
+  const filters = useSelector((state) => state.aviasales.filters);
+
+  const [isAll, setIsAll] = useState(false);
+
+  useEffect(() => {
+    const allChecked = filters.every((filter) => filter.value);
+    setIsAll(allChecked);
+  }, [filters]);
+
+  const toggleAllFilter = (isChecked) => {
+    dispatch(setFilter({ name: 'Все', value: isChecked }));
+    filters.forEach((filter) => {
+      dispatch(setFilter({ name: filter.name, value: isChecked }));
+    });
+  };
+
+  const toggleFilter = (name, isChecked) => {
+    dispatch(setFilter({ name, value: isChecked }));
+  };
+
   return (
     <aside className={styles.filter}>
       <span className={styles.filter__title}>КОЛИЧЕСТВО ПЕРЕСАДОК</span>
       <ul className={styles.filter__items}>
-        <li className={styles.checkbox}>
-          <label className={styles.checkbox__label}>
-            <input className={styles.checkbox__real} type="checkbox" />
-            <span className={styles.checkbox__unreal} />
-            Все
-          </label>
-        </li>
-        <li className={styles.checkbox}>
-          <label className={styles.checkbox__label}>
-            <input className={styles.checkbox__real} type="checkbox" />
-            <span className={styles.checkbox__unreal} />
-            Без пересадок
-          </label>
-        </li>
-        <li className={styles.checkbox}>
-          <label className={styles.checkbox__label}>
-            <input className={styles.checkbox__real} type="checkbox" />
-            <span className={styles.checkbox__unreal} />1 пересадка
-          </label>
-        </li>
-        <li className={styles.checkbox}>
-          <label className={styles.checkbox__label}>
-            <input className={styles.checkbox__real} type="checkbox" />
-            <span className={styles.checkbox__unreal} />2 пересадки
-          </label>
-        </li>
-        <li className={styles.checkbox}>
-          <label className={styles.checkbox__label} >
-            <input className={styles.checkbox__real} type="checkbox" />
-            <span className={styles.checkbox__unreal} />3 пересадки
-          </label>
-        </li>
+        <FilterItem
+          value="Все"
+          label="Все"
+          checked={isAll}
+          onClick={toggleAllFilter}
+        />
+        {filters.map((filter) => (
+          <FilterItem
+            key={filter.name}
+            value={filter.name}
+            label={filter.name}
+            checked={filter.value}
+            onClick={(isChecked, name) => toggleFilter(name, isChecked)}
+          />
+        ))}
       </ul>
     </aside>
   );
 }
+
 export default Filter;
